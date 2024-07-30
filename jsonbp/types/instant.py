db1db5b1927ff56f78c3f2025f25ff40fc2f5231
Gustavo <gustavo.c.venturini@gmail.com>
2024-07-30 05:22:58 -0300

    Added ISO option for instant

--- jsonbp/types/instant.py
+++ jsonbp/types/instant.py
@@ -2,11 +2,18 @@
 from datetime import datetime
 
 _defaults = {
+	'iso': False,
 	'format': "%Y-%m-%dT%H:%M:%S%z",
+	'isoResolution': 'milliseconds'
 }
 
 
 def _format(value, specs):
+	if specs['iso']:
+		resolution = specs['isoResolution']
+		result = value.isoformat(timespec=resolution)
+		return f'"{result}"'
+
 	result = value.strftime(specs['format'])
 	return f'"{result}"'
 
